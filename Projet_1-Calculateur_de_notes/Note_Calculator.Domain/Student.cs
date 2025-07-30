65aad80ca1516cf2a6221b0cf2391da35c36de5a
kechou <lucas.kechouri@outlook.com>
2025-07-30 21:15:48 +0200

    Etape d'implémentation du minimum requis afin de valider le test créer plus tôt dans le projet, valider l'implémentation de la classe en la testant dans notre test rouge, transforme se test en -test vert- ce qui nous permet de nous projeter sur la derniere étape du process TDD: refactoriser le code de manière à l'optiomiser le plus possible.

--- Projet_1-Calculateur_de_notes/Note_Calculator.Domain/Student.cs
+++ Projet_1-Calculateur_de_notes/Note_Calculator.Domain/Student.cs
@@ -1,15 +1,17 @@
 ﻿namespace Note_Calculator.Domain;
 
-//On défini le squelette de notre objet métier.
-//non seulement cela pose la base de notre architecture,
-//mais cela est aussi le minimum pour faire fonctionné le test.
+//--------------------------------------------------------------------------------------------------------
+// Le métier: Maintenant que l'on as posé les bases, on met-à-jour la classe Student afin de passer le test.
+// Cette étape correspond à la phase du "Test Vert" dans le cycle TDD.
+//--------------------------------------------------------------------------------------------------------
+// Rappel du besoin : vérifier si un étudiant valide son année en fonction de la moyenne de ses notes.
+// - Une moyenne >= 10 valide l’année.
+// - L’implémentation suit le principe du TDD : uniquement le code nécessaire pour passer le test vert.
 //
-//Un aspect TRES important est d'uniquement implémenter le MINIMUM possible pour rendre le test fonctionnel.
-//Comme cela on ne perd pas de temps à développer les feature clef et on gagne du temps et des ressources.
-//--------------------------------------------------------------------------------------------------------
-// Le métier: La classe Student dans sa définition la plus basique pour notre besoin actuel
-// (ie. faire fonctionner le test -> on s'attend à ce qu'il nous renvoi une erreur).
-//--------------------------------------------------------------------------------------------------------
+// Remarques :
+// - Les notes sont stockées en interne via un champ privé en lecture seule (_notes).
+// - SetNotes permet l’injection contrôlée des notes via params.
+// - Validate calcule la moyenne et retourne un booléen selon le seuil requis.
 public class Student
 {
     private readonly List<int> _notes = new();
@@ -19,7 +21,13 @@ public class Student
     }
 
     public bool Validate() {
-        //pas besoin de logique interne pour le moment on veux juste rendre le test fonctionnel
-        return false;
+        if (_notes.Count == 0)
+            return false;
+
+        int moyenne = 0;
+        foreach (int note in _notes)
+            moyenne += note;
+
+        return ((double)moyenne / _notes.Count) >= 10; // Cast en double pour éviter l’arrondi entier lors de la division
     }
 }
